705d242921b8ac32f2ba3d3857761fbe0d0656b3
Bryce Monaco <brycedmonaco@gmail.com>
2018-05-15 16:28:59 -0700

    Added Return Button

---
+++ Snowball Fight/Assets/Scripts/ReturnSign.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnSign : MonoBehaviour {
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.CompareTag ("Snowball"))
+        {
+            SceneManager.LoadScene(0);
+
+        }
+
+    }
+}
